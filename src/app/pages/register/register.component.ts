c77bf7e94399048331e369bd8332679332e3766d
Ali najafian <alinajafian580@gmail.com>
2024-03-08 14:27:23 +0330

    Fixed register component and paired it with backend (improvements required)

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtAuthService } from 'src/app/services/JwtAuth/jwt-auth.service';
+import { LoginService } from 'src/app/services/loginService/login.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerUser: RegisterUser = new RegisterUser;
+  repeatedPassword: string;
+  registerMessage: string;
+  repeatedWrong: boolean = false;
 
+
+  constructor(
+    private loginService : LoginService,
+    private jwtService: JwtAuthService,
+    private router: Router
+  ) {}
+
+  registerHandler() {
+    if (this.repeatedPassword == this.registerUser.password) {
+      this.loginService.registerUser(this.registerUser).subscribe(
+        (data: any) => {
+          console.log(data)
+          this.registerMessage = 'Success!'
+          this.jwtService.setJwtToken(data.token)
+          setTimeout(() => {
+            this.router.navigate(['home'])
+          }, 2000);
+        }
+      )
+      
+    } else {
+      this.repeatedWrong = true;
+    }
+  }
+
+}
+
+
+export class RegisterUser {
+  name : string;
+  username: string;
+  email: string;
+  password: string;
+  constructor() {}
 }
--- src/app/services/loginService/login.service.ts
+++ src/app/services/loginService/login.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_URL } from 'src/app/app.constants';
 import { User } from 'src/app/pages/login/login.component';
+import { RegisterUser } from 'src/app/pages/register/register.component';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class LoginService {
 
   logUserIn(user : User) {
     return this.http.post(`${API_URL}/api/v1/auth/authenticate`, user);
+  }
+
+  registerUser(user : RegisterUser) {
+    return this.http.post(`${API_URL}/api/v1/auth/register`, user);
   }
 
 }
