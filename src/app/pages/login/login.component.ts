74c8da3c5b1f0c360a5cf76dd370ca91a09998ed
Ali <alinajafian580@gmail.com>
2025-03-12 10:12:47 +0330

    clean up old code and unused imports

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,6 @@
-import { Component, EventEmitter, OnDestroy, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtAuthService } from 'src/app/services/JwtAuth/jwt-auth.service';
-import { BasicAuthenticationService } from 'src/app/services/hardCodedAuth/basic-auth.service';
-import { HardCodedAuthService } from 'src/app/services/hardCodedAuth/hard-coded-auth.service';
 import { LoginService } from 'src/app/services/loginService/login.service';
 
 @Component({
@@ -21,8 +19,6 @@ export class LoginComponent implements OnInit, OnDestroy {
 
 
   constructor(private router: Router,
-    private hardCodedAuthService: HardCodedAuthService,
-    private basicAuthService: BasicAuthenticationService,
     public loginService : LoginService,
     private jwtService: JwtAuthService) {
 
@@ -53,42 +49,6 @@ export class LoginComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
       
   }
-
-
-  // ===================================================================
-  // This Part is useless prolly will be deleted soon
-
-  // handleLogin(): void {
-  //   if (this.hardCodedAuthService.authenticate(this.username, this.password)) {
-  //     this.router.navigate(['home', this.username]);
-  //   } else {
-  //     this.invalidPass = true;
-  //   }
-  // }
-
-
-
-  // handleBasicAuthLogin(): void {
-  //   this.basicAuthService.executeBasicAuth(this.username, this.password)
-  //     .subscribe(
-  //       data => {
-  //         this.router.navigate(['home', this.username]);
-  //         console.log(data)
-  //       },
-  //       error => this.invalidPass = true
-  //     )
-
-    // if (this.username === "18Kay" && this.password === "18Kay") {
-    //   //navigate to home page
-    //   this.router.navigate(['home', this.username])
-    // } else {
-    //   this.invalidPass = true;
-    // }
-
-    // This Part is useless prolly will be deleted soon  
-    // ===================================================================
-      
-
 
   }
 
